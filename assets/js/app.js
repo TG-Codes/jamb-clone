8c82a4c9bc33a83fc702a5d2fef6faa10ea864b6
OrientzCoder <44888315+OrientzCoder@users.noreply.github.com>
2020-12-07 14:31:57 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+/**
+ * Password Validation
+ */
 $(document).ready(function() {
     $("#password2").keyup(function() {
         if ($("#password").val() != $("#password2").val()) {
@@ -6,6 +9,62 @@ $(document).ready(function() {
         } else {
             $("#StatusPassword").html("Password matched").css("color", "green");
             $("#submit").prop('disabled', false);
+        }
+    });
+});
+
+
+/**
+ * Registration 
+ */
+
+$(document).ready(function() {
+    $("#form").on("submit", function(event) {
+        event.preventDefault();
+
+        var fname = $("#fname").val();
+        var mname = $("#mname").val();
+        var lname = $("#lname").val();
+        var email = $("#email").val();
+        var address = $("#address").val();
+        var state = $("#state").val();
+        var phone = $("#phone").val();
+        var password = $("#password").val();
+
+        if (fname !== "" && mname !== "" && lname !== "" && email !== "" && address !== "" && state !== "" && phone !== "" && password !== "") {
+
+            $.ajax({
+                type: 'POST',
+                url: 'Home/register',
+                data: $("#form").serialize(),
+                dataType: 'json',
+                async: true,
+                beforeSend: function() {
+                    $("#error").fadeOut("slow");
+                    $("#submit").html('Process.......');
+                },
+                success: function(response) {
+                    if (response.error = false) {
+                        $("#error").fadeIn(1000, function() {
+                            $("#error").html('<div class="alert alert-success" role="alert"> ' + response.message + '!</div>');
+                            $("#submit").html('Submit');
+                            document.getElementById("form").reset();
+                        });
+                    } else {
+                        $("#error").fadeIn(1000, function() {
+                            $("#error").html('<div class="alert alert-danger"> ' + response.message + '!</div>');
+                            $("#submit").html('Submit');
+                        });
+                    }
+                }
+
+            });
+
+        } else {
+            $("#error").fadeIn(1000, function() {
+                $("#error").html('<div class="alert alert-danger"> All Fields are required!</div>');
+                $("#submit").html('Submit');
+            });
         }
     });
 });
